119f4fd7fa96cd9c1d505f21b04771f1271e5aa5
László Nagy <nagylaci755@gmail.com>
2024-01-16 18:20:08 +0100

    Added data for auction component

---
+++ src/app/auction-data.ts
@@ -0,0 +1,38 @@
+export const auctionItems = [
+    {
+        id: 1,
+        img: "../assets/img/a1.png",
+        name: "Cathedral of leaves",
+        artist: "Victoria G.",
+        description: "A masterpiece created 12 years ago by an artist whose vision transcends time, 'Cathedral of Leaves' stands as an exquisite testament to the enduring beauty of nature and human creativity. This evocative painting captures the essence of a sacred grove, where the cathedral and the trees intertwines with ethereal brushstrokes.",
+        startingPrice: 1000,
+        currentBid: 1400,
+        bidIncrement: 50,
+        auctionStart: "Jan 3, 2024",
+        auctionEnd: "Jan 31, 2024"
+    },
+    {
+        id: 2,
+        img: "../assets/img/a2.png",
+        name: "Conversations Beyond Mortality",
+        artist: "Antonio F.",
+        description: "Seated at the heart of this transcendent gathering is a middle-aged man, his presence commanding attention. Clad in earthly vulnerability, he wields a sickle – a poignant symbol of the inevitable cycle of life. Grounded yet surrounded by celestial beings, he personifies the delicate balance between mortality and the divine.",
+        startingPrice: 2000,
+        currentBid: 2300,
+        bidIncrement: 100,
+        auctionStart: "Jan 8, 2024",
+        auctionEnd: "Feb 5, 2024"
+    },
+    {
+        id: 3,
+        img: "../assets/img/a3.png",
+        name: "Harmony in Waves: A Melodic Portrait",
+        artist: "Alexis B.",
+        description: "'Harmony in Waves' serves as a testament to the timeless connection between visual and auditory arts, where the brushstrokes create a melodic dance on the canvas. This painting invites you to experience the beauty of music through the eyes of a captivating muse, where every stroke and note coalesce into a harmonious masterpiece.",
+        startingPrice: 3500,
+        currentBid: 4600,
+        bidIncrement: 100,
+        auctionStart: "Jan 10, 2024",
+        auctionEnd: "Feb 10, 2024"
+    }
+];
