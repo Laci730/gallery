7ff08ceaa7a25158f2622088ce1f3699081a15a2
László Nagy <nagylaci755@gmail.com>
2024-01-16 18:20:22 +0100

    Added data for exhibition component

---
+++ src/app/exhibition-data.ts
@@ -0,0 +1,22 @@
+export const exhibitionItems = [
+    {
+        id: 1,
+        img: "../assets/img/e1.png",
+        title: "Underwater Utopia: Marine Life in Brushstrokes"
+    },
+    {
+        id: 2,
+        img: "../assets/img/e2.png",
+        title: "Chronicles of Color: A Palette Exploration"
+    },
+    {
+        id: 3,
+        img: "../assets/img/e3.png",
+        title: ""
+    },
+    {
+        id: 4,
+        img: "../assets/img/e4.png",
+        title: ""
+    }
+]
