02a3b418395ffa961f78678c9081d4a65db4f6a4
László Nagy <nagylaci755@gmail.com>
2024-01-19 16:36:00 +0100

    Created main page component

---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AboutComponent } from '../about/about.component';
+import { AuctionsComponent } from '../auctions/auctions.component';
+import { ExhibitionsComponent } from '../exhibitions/exhibitions.component';
+
+@Component({
+  selector: 'app-main-page',
+  standalone: true,
+  imports: [
+    AboutComponent,
+    AuctionsComponent,
+    ExhibitionsComponent],
+  templateUrl: './main-page.component.html',
+  styleUrl: './main-page.component.css'
+})
+export class MainPageComponent {
+
+}
