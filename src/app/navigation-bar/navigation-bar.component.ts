8dad4fdb77d1719a48c3a5947173e8596b4f863d
László Nagy <nagylaci755@gmail.com>
2024-01-23 15:08:56 +0100

    Created responsive dropdown

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css'
 })
 export class NavigationBarComponent {
+  isMobile = false;
+  isOpened = false;
 
+  constructor(private breakpointService: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointService
+    .observe([Breakpoints.Small, Breakpoints.XSmall])
+    .subscribe((result) => {
+      if (result.matches) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    })
+  }
+
+  changeElement() {
+    this.isOpened = !this.isOpened;
+  }
 }
